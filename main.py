a58d4d6da0840d5c9d5ea8913e7a0bd70d13df5f
Sohan <sohan291106@gmail.com>
2025-11-17 13:36:27 +0530

    added game window

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ashta Cheamma")
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False   
+    pygame.quit()
+    
+if __name__ == "__main__":
+    main()
